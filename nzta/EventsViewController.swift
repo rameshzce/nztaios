8e9fd0fc3755a198e51aff83dbabc90823413c4b
rameshkolamala <rameshzce@gmail.com>
2016-05-21 23:10:45 +0530

    Events

--- nzta/EventsViewController.swift
+++ nzta/EventsViewController.swift
@@ -28,6 +28,7 @@ class EventsViewController: UITableViewController {
         
         tableView.estimatedRowHeight = 95.0
         tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.backgroundColor = hexStringToUIColor("#ff218e")
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -63,6 +64,33 @@ class EventsViewController: UITableViewController {
         //cell.descriptionLabel.text = hotel.description
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+        cell.backgroundColor = hexStringToUIColor("#ff0000")
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
+        
+        if (cString.hasPrefix("#")) {
+            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.grayColor()
+        }
+        
+        var rgbValue:UInt32 = 0
+        NSScanner(string: cString).scanHexInt(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 
     /*
