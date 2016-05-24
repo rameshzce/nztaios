796d9d5a44fe903f334b3fc0997a3a003e81123d
rameshkolamala <rameshzce@gmail.com>
2016-05-24 11:30:23 +0530

    Menu

---
+++ MenuViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MenuViewController.swift
+//  nzta
+//
+//  Created by Ramesh Kolamala on 24/05/16.
+//  Copyright © 2016 tokkalo.com. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+    var menuItems = ["Home", "Profie", "Subscribe", "Team NZTA", "Sponsores"]
+    var currentItem = "Home"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return menuItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! MenuViewCell
+        
+        // Configure the cell...
+        cell.titleLabel.text = menuItems[indexPath.row]
+        cell.titleLabel.textColor = (menuItems[indexPath.row] == currentItem) ? UIColor.whiteColor() : UIColor.grayColor()
+        
+        return cell
+    }
+    
+}
---
+++ nzta/MenuViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MenuViewCell.swift
+//  nzta
+//
+//  Created by Ramesh Kolamala on 24/05/16.
+//  Copyright © 2016 tokkalo.com. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
