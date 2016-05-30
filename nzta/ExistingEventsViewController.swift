165ab58f314ec5a0bcf124d160107f39aac92792
rameshkolamala <rameshzce@gmail.com>
2016-05-30 08:47:45 +0530

    Existing events

--- nzta/ExistingEventsViewController.swift
+++ nzta/ExistingEventsViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ExistingEventsViewController: UIViewController {
+class ExistingEventsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet var datePicker: UIPickerView!
+    
+    var selectedYear: String = ""
+    
+    var Array = ["2012", "2013", "2014", "2015"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        datePicker.delegate = self
+        datePicker.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +29,31 @@ class ExistingEventsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Array.count
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //return row?
+        selectedYear = Array[row]
+    }
+    
+    @IBAction func selectYear(sender: UIButton) {
+        let alertController = UIAlertController(title: "NZTA",
+                                                message: "\(selectedYear)", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion:
+            nil)
+    }
 
     /*
     // MARK: - Navigation
