d7562d10b01945992bf475fbf11d564accc17c13
rameshkolamala <rameshzce@gmail.com>
2016-06-10 23:23:05 +0530

    Sponsors

---
+++ nzta/SponsorsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SponsorsViewCell.swift
+//  nzta
+//
+//  Created by Ramesh Kolamala on 10/06/16.
+//  Copyright © 2016 tokkalo.com. All rights reserved.
+//
+
+import UIKit
+
+class SponsorsViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
